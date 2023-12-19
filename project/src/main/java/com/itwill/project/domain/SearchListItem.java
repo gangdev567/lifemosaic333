c40f9aef90f6e5e93d126b846ae3de4e7601494a
Kim Yeonsu <dustn7020@naver.com>
2023-12-20 00:59:49 +0900

    total search list

---
+++ project/src/main/java/com/itwill/project/domain/SearchListItem.java
@@ -0,0 +1,28 @@
+package com.itwill.project.domain;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchListItem {
+	private Long sub_category_id;
+	private String sub_category_name;
+	private Long post_id;
+	private String title;
+	private String content;
+	private String nickname;
+	private String user_id;
+	private Long view_count;
+	private Long like_point;
+	private Long comment_cnt;
+	private String created_time;
+
+}
